07f887afeda6117211da3ff3bad29536ae932954
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 08:36:37 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" A module for fetching data using RESTFUL API"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    id = sys.argv[1]
+    user_id = sys.argv[1]
     user_data = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                              .format(sys.argv[1])).json()
-    username = user_data.get("username")
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
                          params={"userId": sys.argv[1]}).json()
+    username = user_data.get("username")
 
-    with open("{}.csv".format(id), "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [id, username, task.get("completed"), task.get("title")]
-         ) for task in tasks]
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in tasks]

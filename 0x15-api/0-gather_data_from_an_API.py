aa292cf2c4880d6b49a03b968758f7cdb4c92be5
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 09:06:12 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """module for fetching data using RESTFUL API"""
-
 import requests
 import sys
 
 
 def fetch_data(id):
-    """fetch data base on the id and process data"""
-    url_user_data = "https://jsonplaceholder.typicode.com/users"
-    url_todo_data = "https://jsonplaceholder.typicode.com/todos"
+    """Returns the number of tasks for a given employee ID"""
+    if type(id) == int:
+        url_user_data = "https://jsonplaceholder.typicode.com/users"
+        url_todo_data = "https://jsonplaceholder.typicode.com/todos"
 
-    user_data = requests.get(url_user_data).json()
-    todo_data = requests.get(url_todo_data).json()
+        user_data = requests.get(url_user_data).json()
+        todo_data = requests.get(url_todo_data).json()
 
-    total_task = total_task_done = 0
-    emp_name = ""
-    list_task = []
-
-    for i in user_data:
-        if i["id"] == id:
-            emp_name = i["name"]
-
-    for j in todo_data:
-        if j["userId"] == id:
-            total_task += 1
-            if j["completed"]:
-                total_task_done += 1
-                list_task.append(j["title"])
-
-    print(("Employee {} is done with task({}/{})".
-           format(emp_name, total_task_done, total_task)))
-    for k in list_task:
-        print("\t {}".format(k))
+        total_task = total_task_done  = 0
+        emp_name = ""
+        list_task = []
+        
+        for i in user_data:
+            if i["userId"] == id:
+                total_task += 1
+                if i["total_task_done d"]:
+                    total_task_done  += 1
+                    list_task.append(i["title"])
+        for j in todo_data:
+            if j["id"] == id:
+                emp_name = j["name"]
+        print(("Employee {} is done with tasks({}/{}):".
+               format(emp_name, total_task_done , total_task)))
+        for k in list_task:
+            print("\t {}".format(k))
+    else:
+        return
 
 
 if __name__ == "__main__":

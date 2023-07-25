4202b20da1d7e23232319c7177aa9974fe249b13
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 10:28:16 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,23 +12,27 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    url_usr_data = "https://jsonplaceholder.typicode.com/users"
-    user_data = url_usr_data + "/" + employee_id
+    initial_url = "https://jsonplaceholder.typicode.com/users"
+    url = initial_url + "/" + employee_id
 
-    response = requests.get(user_data)
-    todo_data = requests.get(user_data + "/todos")
+    response = requests.get(url)
+    todolist_data = requests.get(url + "/todos")
+    """print(todolist_data.json())"""
 
-    emp_name = response.json().get("name")
-    total_task = todo_data.json()
+    e_name = response.json().get("name")
+    """print(e_name)"""
+    all_tasks = todolist_data.json()
+    """print(all_tasks)"""
 
-    completed = 0
-    task_list = []
-    for task in total_task:
+    done = 0
+    done_tasks = []
+    for task in all_tasks:
         if task.get('completed'):
-            task_list.append(task)
-            completed += 1
+            done_tasks.append(task)
+            done += 1
+    """print(done_tasks)"""
     print("Employee {} is done with tasks({}/{}):".format(
-        emp_name, len(task_list), len(total_task)))
+        e_name, len(done_tasks), len(all_tasks)))
 
-    for task in task_list:
+    for task in done_tasks:
         print("\t {}".format(task.get('title')))

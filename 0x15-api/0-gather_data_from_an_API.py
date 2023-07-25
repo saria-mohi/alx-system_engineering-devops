ad8694b8c68e82ce5d06f86018512d407e0724d0
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 07:08:29 +0300

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""module for fetching data using RESTFUL API"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
-def fetch_data(id):
-    """fetch data base on the id and process data"""
-    url_user_data = "https://jsonplaceholder.typicode.com/users"
-    url_todo_data = "https://jsonplaceholder.typicode.com/todos"
-
-    user_data = requests.get(url_user_data).json()
-    todo_data = requests.get(url_todo_data).json()
-
-    total_task = total_task_done = 0
-    emp_name = ""
-    list_task = []
-
-    for i in user_data:
-        if i["id"] == id:
-            emp_name = i["name"]
-
-    for j in todo_data:
-        if j["userId"] == id:
-            total_task += 1
-            if j["completed"]:
-                total_task_done += 1
-                list_task.append(j["title"])
-
-    print(("Employee {} is done with task({}/{})".
-           format(emp_name, total_task_done, total_task)))
-    for k in list_task:
-        print("\t {}".format(k))
-
-
 if __name__ == "__main__":
-    fetch_data(int(sys.argv[1]))
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]

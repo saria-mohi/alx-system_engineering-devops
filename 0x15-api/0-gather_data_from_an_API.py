e14a97842abbc7409e0508b87ed0af2cfee2d6bc
saria-mohi <saria.m.mohammed@gmail.com>
2023-07-24 20:30:07 -0700

    minor style update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,12 @@
-#!/bin/bash/python3
-"""A module for fetching data using RESTFUL API"""
+#!/usr/bin/python3
+"""module for fetching data using RESTFUL API"""
 
 import requests
 import sys
 
 
 def fetch_data(id):
-    """fetch data base on the id and process data""""
+    """fetch data base on the id and process data"""
     url_user_data = "https://jsonplaceholder.typicode.com/users"
     url_todo_data = "https://jsonplaceholder.typicode.com/todos"
 
@@ -17,20 +17,17 @@ def fetch_data(id):
     emp_name = ""
     list_task = []
 
-    # lets get employee name
     for i in user_data:
         if i["id"] == id:
             emp_name = i["name"]
 
-    # fetch todo data
     for j in todo_data:
         if j["userId"] == id:
             total_task += 1
             if j["completed"]:
-                total_taks_done += 1
+                total_task_done += 1
                 list_task.append(j["title"])
 
-    # Employee Ervin Howell is done with tasks(8/20):
     print(("Employee {} is done with task({}/{})".
            format(emp_name, total_task_done, total_task)))
     for k in list_task:

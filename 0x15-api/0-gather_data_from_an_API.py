a9554f65e09e4282e0cb41b89e6c19f5a668728d
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 07:38:53 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+""" A module for fetching data using RESTFUL API"""
 import requests
 import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user_data = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos_task = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [task.get("title") for task in todos_task if task.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
+        user_data.get("name"), len(completed), len(todos_task)))
     [print("\t {}".format(c)) for c in completed]

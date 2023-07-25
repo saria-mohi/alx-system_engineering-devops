30502272d7c7fdb828ff2d41848fc7947f9bf067
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 08:43:58 +0300

    file 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
-""" A module for fetching data using RESTFUL API"""
+"""module for fetching data using RESTFUL API"""
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_data = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                             .format(sys.argv[1])).json()
-    tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
-                         params={"userId": sys.argv[1]}).json()
 
-    completed = [task.get("title") for task in tasks if task.
-                 get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(completed), len(tasks)))
-    [print("\t {}".format(c)) for c in completed]
+def fetch_data(id):
+    """fetch data base on the id and process data"""
+    url_user_data = "https://jsonplaceholder.typicode.com/users"
+    url_todo_data = "https://jsonplaceholder.typicode.com/todos"
+
+    user_data = requests.get(url_user_data).json()
+    todo_data = requests.get(url_todo_data).json()
+
+    total_task = total_task_done = 0
+    emp_name = ""
+    list_task = []
+
+    for i in user_data:
+        if i["id"] == id:
+            emp_name = i["name"]
+
+    for j in todo_data:
+        if j["userId"] == id:
+            total_task += 1
+            if j["completed"]:
+                total_task_done += 1
+                list_task.append(j["title"])
+
+    print(("Employee {} is done with task({}/{})".
+           format(emp_name, total_task_done, total_task)))
+    for k in list_task:
+        print("\t {}".format(k))
+
+
+if __name__ == "__main__":
+    fetch_data(int(sys.argv[1]))
